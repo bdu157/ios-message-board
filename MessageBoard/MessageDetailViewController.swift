1994f72249995deeb7aa2edad41420ad510ab973
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-28 17:29:13 -0700

    set up part1 - sotryboard layout

---
+++ MessageBoard/MessageDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MessageDetailViewController.swift
+//  MessageBoard
+//
+//  Created by Dongwoo Pae on 5/28/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func sendButtonTapped(_ sender: Any) {
+    }
+}
